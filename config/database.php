e07d9e9972a9ffc55282f34b2bb9cc1c767ff78e
Yan Gonçalves <yanw100@gmail.com>
2024-03-09 17:20:26 -0300

    Ajusta conexão do mongo

--- config/database.php
+++ config/database.php
@@ -98,8 +98,8 @@ return [
             'host'     => env('MONGO_DB_HOST', 'localhost'),
             'port'     => env('MONGO_DB_PORT', 27017),
             'database' => env('MONGO_DB_DATABASE'),
-            'username' => env('MONGO_DB_USERNAME', ''),
-            'password' => env('MONGO_DB_PASSWORD', ''),
+            'username' => env('MONGO_DB_USERNAME', 'todo'),
+            'password' => env('MONGO_DB_PASSWORD', 'todo'),
             'options'  => []
         ],
     ],
